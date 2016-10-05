61a65e96982188d200151d9c34b645f7a3eca92b
Anders Poulsen <apo@elonordic.com>
2016-10-05 16:16:03 +0200

    Updated CMkake file to include the -ggdb flag and update flash.bat to open openocd in a new window

--- flash.bat
+++ flash.bat
@@ -1,2 +1,3 @@
 @echo off
-openocd -s "%OpenOCD%\scripts" -f "%OpenOCD%\scripts\board\stm32f4discovery.cfg" -c "init" -c "reset halt" -c "flash write_image erase ./build/DemoBoard" -c "reset run" 
+taskkill /F /IM openocd.exe
+start "OpenOCD" cmd /C openocd -s "%OpenOCD%\scripts" -f "%OpenOCD%\scripts\board\stm32f4discovery.cfg" -c "init" -c "reset halt" -c "flash write_image erase ./build/DemoBoard" -c "reset halt" 
