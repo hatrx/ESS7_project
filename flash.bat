18a1d4eda7609d44780f464c29807e067c8eac29
Anders Poulsen <apo@elonordic.com>
2016-11-02 10:19:24 +0100

    Updated windows build and flash files to be compatible with VS Code build tasks

--- flash.bat
+++ flash.bat
@@ -1,3 +1,14 @@
 @echo off
 taskkill /F /IM openocd.exe
-start "OpenOCD" cmd /C openocd -s "%OpenOCD%\scripts" -f "%OpenOCD%\scripts\board\stm32f4discovery.cfg" -c "init" -c "reset halt" -c "flash write_image erase ./build/DemoBoard" -c "reset halt" 
+if "%1"=="--embed" goto :embed
+goto :noembed
+
+:embed
+cmd /C openocd -s "%OpenOCD%\scripts" -f "%OpenOCD%\scripts\board\stm32f4discovery.cfg" -c "init" -c "reset halt" -c "flash write_image erase ./build/DemoBoard" -c "reset halt"
+goto :exit
+
+:noembed
+start "OpenOCD" cmd /C openocd -s "%OpenOCD%\scripts" -f "%OpenOCD%\scripts\board\stm32f4discovery.cfg" -c "init" -c "reset halt" -c "flash write_image erase ./build/DemoBoard" -c "reset halt"
+
+:exit
+if "%2"=="--exit-on-finish" EXIT
--- winbuild.bat
+++ winbuild.bat
@@ -2,3 +2,5 @@
 cd build
 bash -c "make"
 cd ..
+
+if "%1"=="--exit-on-finish" EXIT
