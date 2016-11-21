6c6168eb870cf0e61b557950c86d9c227f09b33b
Gabriel <gvaslu16@student.aau.dk>
2016-11-21 21:06:42 +0100

    RTC: passing a struct to the main function

--- src/drivers/rtc.c
+++ src/drivers/rtc.c
@@ -11,41 +11,70 @@ RTC_DateTypeDef RTCdate;
 void HAL_RTC_MspInit(RTC_HandleTypeDef *RTCHandle)
 {
 	(void)RTCHandle;
-
+	__HAL_RCC_PWR_CLK_ENABLE();
+	HAL_PWR_EnableBkUpAccess();
+	__HAL_RCC_RTC_CONFIG(RCC_RTCCLKSOURCE_LSE);
+	__HAL_RCC_RTC_ENABLE();
 }
 //write brief
 void BSP_RTC_init()
 {
-	__HAL_RCC_PWR_CLK_ENABLE();
-	HAL_PWR_EnableBkUpAccess();
-	
-	__HAL_RCC_RTC_CONFIG(RCC_RTCCLKSOURCE_LSE);
-	__HAL_RCC_RTC_ENABLE();
-
 	RTCHandle.Instance = RTC;
-	
 	RTCHandle.Init.HourFormat = RTC_HOURFORMAT_24;
-	RTCHandle.Init.AsynchPrediv = 124;
-	RTCHandle.Init.SynchPrediv = 7999;
+	RTCHandle.Init.AsynchPrediv = 127;
+	RTCHandle.Init.SynchPrediv = 257;
+	//The asynchronous prescaler division factor is set to 128, and the synchronous division
+	//factor to 256, to obtain an internal clock frequency of 1 Hz (ck_spre) with an LSE frequency
+	//of 32.768 kHz.
 	RTCHandle.Init.OutPut = RTC_OUTPUT_DISABLE;
 	RTCHandle.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
 	RTCHandle.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
-	
+
 	HAL_RTC_Init(&RTCHandle);
 
-	RTCtime.Hours = 0x0C;
-	RTCtime.Minutes = 0x00;
-	RTCtime.Seconds = 0x00;
-	RTCtime.SubSeconds = 0x00;
+	RTCtime.Hours = 12;
+	RTCtime.Minutes = 15;
+	RTCtime.Seconds = 00;
+	RTCtime.SubSeconds = 10;
 	RTCtime.TimeFormat = RTC_HOURFORMAT_24;
 	RTCtime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
 	RTCtime.StoreOperation = RTC_STOREOPERATION_RESET;
-	HAL_RTC_SetTime(&RTCHandle, &RTCtime, FORMAT_BCD);
+	HAL_RTC_SetTime(&RTCHandle, &RTCtime, FORMAT_BIN);
 
 	RTCdate.WeekDay = RTC_WEEKDAY_MONDAY;
 	RTCdate.Month = RTC_MONTH_NOVEMBER;
-	RTCdate.Date = 0x15;
-	RTCdate.Year = 0x10;
-	HAL_RTC_SetDate(&RTCHandle, &RTCdate, FORMAT_BCD);
+	RTCdate.Date = 21;
+	RTCdate.Year = 16;
+	HAL_RTC_SetDate(&RTCHandle, &RTCdate, FORMAT_BIN);
+	
+}
 
+void get_datetime(struct date* now)
+{
+	HAL_RTC_GetTime(&RTCHandle, &RTCtime, FORMAT_BIN);
+	HAL_RTC_GetDate(&RTCHandle, &RTCdate, FORMAT_BIN);
+	now->seconds = RTCtime.Seconds;
+	now->minutes = RTCtime.Minutes;
+	now->hours = RTCtime.Hours;
+	
+
+	/*DateTime now;
+	now.subseconds = RTCtime.SubSeconds;
+	now.seconds = RTCtime.Seconds;
+	now.minutes = RTCtime.Minutes;
+	now.hours = RTCtime.Hours;
+	now.date = RTCdate.Date;
+	now.month = RTCdate.Month;
+	now.year = RTCdate.Year;
+	return now;*/
+}
+/*
+void print_datetime()
+{
+	printf("test");
+}*/
+
+int RTC_state()
+{
+	return HAL_RTC_GetState(&RTCHandle) == HAL_RTC_STATE_READY ? 1 : 0;
 }
--- src/drivers/rtc.h
+++ src/drivers/rtc.h
@@ -1,7 +1,19 @@
 #ifndef RTC_H
 #define RTC_H
 
+struct date
+{
+	uint8_t seconds;
+	uint8_t minutes;
+	uint8_t hours;
+};
+
+
 void HAL_RTC_MspInit(RTC_HandleTypeDef *RTCHandle);
 void BSP_RTC_init();
+
+void get_datetime(struct date*);
+//void print_datetime();
+int RTC_state();
 
 #endif /* UART_H */
