b97e44d2aeb754e5b3356360dd263b2c333e7200
Jakob Østergaard Jensen <jakob.jensen.91@gmail.com>
2016-11-29 10:52:26 +0100

    APEX: Style edits in the relavant APEX headers + their includes

--- third_party/ARINC/APEX/apex_partition.h
+++ third_party/ARINC/APEX/apex_partition.h
@@ -3,46 +3,50 @@
 /* PARTITION constant and type definitions and management services*/
 /*                                                                */
 /*----------------------------------------------------------------*/
- 
-#ifndef APEX_PARTITION #define APEX_PARTITION 
- 
-#define  MAX_NUMBER_OF_PARTITIONS  SYSTEM_LIMIT_NUMBER_OF_PARTITIONS 
- 
-typedef
-    enum {
-        IDLE       = 0,
-        COLD_START = 1,
-        WARM_START = 2,
-        NORMAL     = 3
-    } OPERATING_MODE_TYPE; 
- 
-typedef  APEX_INTEGER    PARTITION_ID_TYPE; 
- 
-typedef
-    enum {
-        NORMAL_START         = 0,
-        PARTITION_RESTART    = 1,
-        HM_MODULE_RESTART    = 2,
-        HM_PARTITION_RESTART = 3
-    } START_CONDITION_TYPE; 
- 
-typedef
-    struct {
-        SYSTEM_TIME_TYPE     PERIOD;
-        SYSTEM_TIME_TYPE     DURATION;
-        PARTITION_ID_TYPE    IDENTIFIER;
-        LOCK_LEVEL_TYPE      LOCK_LEVEL;
-        OPERATING_MODE_TYPE  OPERATING_MODE;
-        START_CONDITION_TYPE START_CONDITION;
-    } PARTITION_STATUS_TYPE; 
- 
- 
+
+#ifndef APEX_PARTITION
+#define APEX_PARTITION
+
+
+#include "apex_types.h"
+#include "apex_process.h"
+
+
+#define  MAX_NUMBER_OF_PARTITIONS  SYSTEM_LIMIT_NUMBER_OF_PARTITIONS
+
+typedef enum {
+	IDLE       = 0,
+	COLD_START = 1,
+	WARM_START = 2,
+	NORMAL     = 3
+} OPERATING_MODE_TYPE;
+
+typedef  APEX_INTEGER    PARTITION_ID_TYPE;
+
+typedef enum {
+	NORMAL_START         = 0,
+	PARTITION_RESTART    = 1,
+	HM_MODULE_RESTART    = 2,
+	HM_PARTITION_RESTART = 3
+} START_CONDITION_TYPE;
+
+typedef struct {
+	SYSTEM_TIME_TYPE     PERIOD;
+	SYSTEM_TIME_TYPE     DURATION;
+	PARTITION_ID_TYPE    IDENTIFIER;
+	LOCK_LEVEL_TYPE      LOCK_LEVEL;
+	OPERATING_MODE_TYPE  OPERATING_MODE;
+	START_CONDITION_TYPE START_CONDITION;
+} PARTITION_STATUS_TYPE;
+
+
 extern void GET_PARTITION_STATUS (
-    /*out*/ PARTITION_STATUS_TYPE    *PARTITION_STATUS,
-    /*out*/ RETURN_CODE_TYPE         *RETURN_CODE ); 
- 
+	/*out*/ PARTITION_STATUS_TYPE    *PARTITION_STATUS,
+	/*out*/ RETURN_CODE_TYPE         *RETURN_CODE );
+
 extern void SET_PARTITION_MODE (
-    /*in */ OPERATING_MODE_TYPE      OPERATING_MODE,
-    /*out*/ RETURN_CODE_TYPE         *RETURN_CODE ); 
- 
-#endif 
+	/*in */ OPERATING_MODE_TYPE      OPERATING_MODE,
+	/*out*/ RETURN_CODE_TYPE         *RETURN_CODE );
+
+
+#endif
--- third_party/ARINC/APEX/apex_queuing.h
+++ third_party/ARINC/APEX/apex_queuing.h
@@ -3,57 +3,62 @@
 /* QUEUING PORT constant and type definitions and management services */
 /*                                                                    */
 /*--------------------------------------------------------------------*/
- 
+
 #ifndef APEX_QUEUING
-#define APEX_QUEUING 
- 
-#define  MAX_NUMBER_OF_QUEUING_PORTS    SYSTEM_LIMIT_NUMBER_OF_QUEUING_PORTS 
- 
-typedef  NAME_TYPE       QUEUING_PORT_NAME_TYPE; 
- 
-typedef  APEX_INTEGER    QUEUING_PORT_ID_TYPE; 
- 
-typedef
-    struct {
-        MESSAGE_RANGE_TYPE      NB_MESSAGE;
-        MESSAGE_RANGE_TYPE      MAX_NB_MESSAGE;
-        MESSAGE_SIZE_TYPE       MAX_MESSAGE_SIZE;
-        PORT_DIRECTION_TYPE     PORT_DIRECTION;
-        WAITING_RANGE_TYPE      WAITING_PROCESSES;
-    } QUEUING_PORT_STATUS_TYPE; 
- 
- 
+#define APEX_QUEUING
+
+
+#include "apex_types.h"
+#include "apex_process.h"
+
+
+#define  MAX_NUMBER_OF_QUEUING_PORTS    SYSTEM_LIMIT_NUMBER_OF_QUEUING_PORTS
+
+typedef  NAME_TYPE       QUEUING_PORT_NAME_TYPE;
+
+typedef  APEX_INTEGER    QUEUING_PORT_ID_TYPE;
+
+typedef struct {
+	MESSAGE_RANGE_TYPE      NB_MESSAGE;
+	MESSAGE_RANGE_TYPE      MAX_NB_MESSAGE;
+	MESSAGE_SIZE_TYPE       MAX_MESSAGE_SIZE;
+	PORT_DIRECTION_TYPE     PORT_DIRECTION;
+	WAITING_RANGE_TYPE      WAITING_PROCESSES;
+} QUEUING_PORT_STATUS_TYPE;
+
+
 extern void CREATE_QUEUING_PORT (
-    /*in */ QUEUING_PORT_NAME_TYPE   QUEUING_PORT_NAME,
-    /*in */ MESSAGE_SIZE_TYPE        MAX_MESSAGE_SIZE,
-    /*in */ MESSAGE_RANGE_TYPE       MAX_NB_MESSAGE,
-    /*in */ PORT_DIRECTION_TYPE      PORT_DIRECTION,
-    /*in */ QUEUING_DISCIPLINE_TYPE  QUEUING_DISCIPLINE,
-    /*out*/ QUEUING_PORT_ID_TYPE     *QUEUING_PORT_ID,
-    /*out*/ RETURN_CODE_TYPE         *RETURN_CODE ); 
- 
+	/*in */ QUEUING_PORT_NAME_TYPE   QUEUING_PORT_NAME,
+	/*in */ MESSAGE_SIZE_TYPE        MAX_MESSAGE_SIZE,
+	/*in */ MESSAGE_RANGE_TYPE       MAX_NB_MESSAGE,
+	/*in */ PORT_DIRECTION_TYPE      PORT_DIRECTION,
+	/*in */ QUEUING_DISCIPLINE_TYPE  QUEUING_DISCIPLINE,
+	/*out*/ QUEUING_PORT_ID_TYPE     *QUEUING_PORT_ID,
+	/*out*/ RETURN_CODE_TYPE         *RETURN_CODE );
+
 extern void SEND_QUEUING_MESSAGE (
-    /*in */ QUEUING_PORT_ID_TYPE     QUEUING_PORT_ID,
-    /*in */ MESSAGE_ADDR_TYPE        MESSAGE_ADDR,       /* by reference */
-    /*in */ MESSAGE_SIZE_TYPE        LENGTH,
-    /*in */ SYSTEM_TIME_TYPE         TIME_OUT,
-    /*out*/ RETURN_CODE_TYPE         *RETURN_CODE); 
- 
+	/*in */ QUEUING_PORT_ID_TYPE     QUEUING_PORT_ID,
+	/*in */ MESSAGE_ADDR_TYPE        MESSAGE_ADDR,       /* by reference */
+	/*in */ MESSAGE_SIZE_TYPE        LENGTH,
+	/*in */ SYSTEM_TIME_TYPE         TIME_OUT,
+	/*out*/ RETURN_CODE_TYPE         *RETURN_CODE);
+
 extern void RECEIVE_QUEUING_MESSAGE (
-    /*in */ QUEUING_PORT_ID_TYPE     QUEUING_PORT_ID,
-    /*in */ SYSTEM_TIME_TYPE         TIME_OUT,
-    /*out*/ MESSAGE_ADDR_TYPE        MESSAGE_ADDR,
-    /*out*/ MESSAGE_SIZE_TYPE        *LENGTH,
-    /*out*/ RETURN_CODE_TYPE         *RETURN_CODE ); 
- 
+	/*in */ QUEUING_PORT_ID_TYPE     QUEUING_PORT_ID,
+	/*in */ SYSTEM_TIME_TYPE         TIME_OUT,
+	/*out*/ MESSAGE_ADDR_TYPE        MESSAGE_ADDR,
+	/*out*/ MESSAGE_SIZE_TYPE        *LENGTH,
+	/*out*/ RETURN_CODE_TYPE         *RETURN_CODE );
+
 extern void GET_QUEUING_PORT_ID (
-    /*in */ QUEUING_PORT_NAME_TYPE   QUEUING_PORT_NAME,
-    /*out*/ QUEUING_PORT_ID_TYPE     *QUEUING_PORT_ID,
-    /*out*/ RETURN_CODE_TYPE         *RETURN_CODE ); 
- 
+	/*in */ QUEUING_PORT_NAME_TYPE   QUEUING_PORT_NAME,
+	/*out*/ QUEUING_PORT_ID_TYPE     *QUEUING_PORT_ID,
+	/*out*/ RETURN_CODE_TYPE         *RETURN_CODE );
+
 extern void GET_QUEUING_PORT_STATUS (
-    /*in */ QUEUING_PORT_ID_TYPE     QUEUING_PORT_ID,
-    /*out*/ QUEUING_PORT_STATUS_TYPE *QUEUING_PORT_STATUS,
-    /*out*/ RETURN_CODE_TYPE         *RETURN_CODE ); 
- 
-#endif 
+	/*in */ QUEUING_PORT_ID_TYPE     QUEUING_PORT_ID,
+	/*out*/ QUEUING_PORT_STATUS_TYPE *QUEUING_PORT_STATUS,
+	/*out*/ RETURN_CODE_TYPE         *RETURN_CODE );
+
+
+#endif
