b91ed0ac10a7463b6765b56978ee5ca0cd179288
Anders Poulsen <apo@elonordic.com>
2016-11-30 13:25:31 +0100

    Fixed the GET_TIME SVC; now working!

--- src/partitions/dummy2/dummy2.c
+++ src/partitions/dummy2/dummy2.c
@@ -8,9 +8,10 @@ void dummy2_main(void)
 {
 	SYSTEM_TIME_TYPE timeNow;
 	RETURN_CODE_TYPE returnCode;
+
 	while (1) {
 		onboard_led_toggle(red_led);
 		delay_ms(50);
-		GET_TIME(&returnCode, &timeNow);
+		GET_TIME(&timeNow, &returnCode);
 	}
 }
--- src/partitions/utils/apex.c
+++ src/partitions/utils/apex.c
@@ -1,4 +1,5 @@
 #include <apex_process.h>
+#include <stm32f4xx_hal.h>
 
 void CREATE_PROCESS (
     /*in */ PROCESS_ATTRIBUTE_TYPE   *ATTRIBUTES,
@@ -7,8 +8,9 @@ void CREATE_PROCESS (
 {
     __asm volatile (
         "MOV    R1, %0              \n\t"
-        "MOV    R0, #0x0000000a     \n\t"
-        "SVC                        \n\t"
+        "MOVT   R0, #0xc0ff         \n\t"
+        "MOVW   R0, #0xee0a         \n\t"
+        "SVC    #0xa                \n\t"
         "NOP                        \n\t"
         "MOV    %[RETURN], R0       \n\t"
         "MOV    %[PID], R1          \n\t"
@@ -22,14 +24,18 @@ void GET_TIME (
     /*out*/ SYSTEM_TIME_TYPE         *SYSTEM_TIME,
     /*out*/ RETURN_CODE_TYPE         *RETURN_CODE )
 {
-    __asm volatile (
-        "MOV    R0, #0xc0ffee0b     \n\t"
-        "SVC                        \n\t"
-        "NOP                        \n\t"
+    uint32_t time_lo, time_hi;
+    __ASM volatile (
+        "MOVW   R0, #0xee0b         \n\t"
+        "MOVT   R0, #0xc0ff         \n\t"
+        "MOVW   R1, #0xface         \n\t"
+        "SVC    #0x0F               \n\t"
         "MOV    %[RETURN], R0       \n\t"
-        "MOV    %[TIME], R1         \n\t"
-        : [RETURN] "=r" (*RETURN_CODE), [TIME] "=r" (*SYSTEM_TIME)
+        "MOV    %[TIME_LO], R1      \n\t"
+        "MOV    %[TIME_HI], R2      \n\t"
+        : [RETURN] "=r" (*RETURN_CODE), [TIME_LO] "=r" (time_lo), [TIME_HI] "=r" (time_hi)
         :
         :
     );
+    *SYSTEM_TIME = (uint64_t) time_lo | (((uint64_t)time_hi) << 32);
 }
