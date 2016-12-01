c2c70b6f258e4873b9fccbda027e788c1b1857a4
Anders Poulsen <apo@elonordic.com>
2016-12-01 11:40:47 +0100

    Removed lines from partitions that required priviledge

--- src/partitions/dummy1/dummy1.c
+++ src/partitions/dummy1/dummy1.c
@@ -11,16 +11,16 @@ void dummy1_main(void)
 {
 	QUEUING_PORT_ID_TYPE QUEUING_PORT_ID;
 	RETURN_CODE_TYPE RETURN_CODE;
-	CREATE_QUEUING_PORT("print_1", 32, 32, SOURCE, FIFO, &QUEUING_PORT_ID, &RETURN_CODE);
+	//CREATE_QUEUING_PORT("print_1", 32, 32, SOURCE, FIFO, &QUEUING_PORT_ID, &RETURN_CODE);
 
 
 	while (1) {
 		char *str = "Dummy 1\n\r";
-		size_t len = strlen(str);
+		//size_t len = strlen(str);
 		RETURN_CODE_TYPE RETURN_CODE;
-		SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;
-		SEND_QUEUING_MESSAGE(QUEUING_PORT_ID, (uint8_t *)str, len, 0, &RETURN_CODE);
-		SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
+		//SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;
+		//SEND_QUEUING_MESSAGE(QUEUING_PORT_ID, (uint8_t *)str, len, 0, &RETURN_CODE);
+		//SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
 
 		for (size_t i = 0; i < 8; i++) {
 			onboard_led_toggle(yellow_led);
--- src/partitions/stdio_sys/stdio_sys.c
+++ src/partitions/stdio_sys/stdio_sys.c
@@ -12,17 +12,17 @@ void stdio_sys_main(void)
 {
 	QUEUING_PORT_ID_TYPE QUEUING_PORT_ID;
 	RETURN_CODE_TYPE RETURN_CODE;
-	CREATE_QUEUING_PORT("sys_stio", 32, 32, DESTINATION, FIFO, &QUEUING_PORT_ID, &RETURN_CODE);
+	//CREATE_QUEUING_PORT("sys_stio", 32, 32, DESTINATION, FIFO, &QUEUING_PORT_ID, &RETURN_CODE);
 
 	char str[1024];
 	while(1) {
 		RETURN_CODE_TYPE RETURN_CODE;
 		MESSAGE_SIZE_TYPE len;
-		SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;
-		RECEIVE_QUEUING_MESSAGE(QUEUING_PORT_ID, 0, (uint8_t *)str, &len, &RETURN_CODE);
-		SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
+		//SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;
+		//RECEIVE_QUEUING_MESSAGE(QUEUING_PORT_ID, 0, (uint8_t *)str, &len, &RETURN_CODE);
+		//SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
 		if (RETURN_CODE == NO_ERROR) {
-			BSP_UARTx_transmit((uint8_t *)str, len);
+			//BSP_UARTx_transmit((uint8_t *)str, len);
 		}
 
 		HAL_Delay(10);
