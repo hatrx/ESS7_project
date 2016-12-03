b735849114b8fe3261e0bbde45b086dba37c6660
Anders Poulsen <apouls16@student.aau.dk>
2016-12-03 15:10:16 +0100

    Removed data and structs from statics that are defined in xml_data.c and types.h

--- src/kernel/arinc/statics.c
+++ src/kernel/arinc/statics.c
@@ -4,60 +4,6 @@
 #include "statics.h"
 #include "queuing_port.h"
 
-
-port_t p_dummy_1[1] = {{
-	.is_queuing_port = true,
-	.q_buf = {
-		MESSAGE_BUFFER(32, 32),
-	},
-	.PORT_DIRECTION = SOURCE,
-	.portname = "print_1",
-},};
-
-port_t p_dummy_2[1] = {{
-	.is_queuing_port = true,
-	.q_buf = {
-		MESSAGE_BUFFER(32, 32),
-	},
-	.PORT_DIRECTION = SOURCE,
-	.portname = "print_2",
-},};
-
-port_t p_stio_sys[1] = {{
-	.is_queuing_port = true,
-	.q_buf = {
-		MESSAGE_BUFFER(32, 32),
-	},
-	.PORT_DIRECTION = DESTINATION,
-	.portname = "sys_stio",
-},};
-
-partition_t partitions[3] = {{
-	.IDENTIFIER = 1,
-	.partitionname = "dummy_1",
-	.criticality = LEVEL_A,
-	.systempartion = false,
-	.entrypoint = &dummy1_main,
-	.nb_ports = 1,
-	.ports = p_dummy_1,
-},{
-	.IDENTIFIER = 2,
-	.partitionname = "dummy_2",
-	.criticality = LEVEL_A,
-	.systempartion = false,
-	.entrypoint = &dummy2_main,
-	.nb_ports = 1,
-	.ports = p_dummy_2,
-},{
-	.IDENTIFIER = 3,
-	.partitionname = "stio_sys",
-	.criticality = LEVEL_A,
-	.systempartion = true,
-	.entrypoint = &stdio_sys_main,
-	.nb_ports = 1,
-	.ports = p_stio_sys,
-},};
-
 mem_req_t mem_dummy_1[2] = {{
 	.type = CODE,
 	.size = 8000,
@@ -111,7 +57,7 @@ part_mem_t partition_memory[3] = {{
 	.memory_arr = mem_stdio_sys,
 },};
 
-port_t *stio_channel_ports[3] = {
+/*port_t *stio_channel_ports[3] = {
 	&p_dummy_1[0],
 	&p_dummy_2[0],
 	&p_stio_sys[0],
@@ -123,4 +69,4 @@ channel_t connection_table[1] = {{
 		.nb_ports = 3,
 		.ports = stio_channel_ports,
 	},
-};
+};*/
