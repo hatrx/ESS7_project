8574b3c96204d070be56e2ea66f6ddcb3f013a40
Jakob Østergaard Jensen <jakob.jensen.91@gmail.com>
2016-12-04 16:14:33 +0100

    xml: updated XML data template

--- xml_data_template.h
+++ xml_data_template.h
@@ -6,8 +6,11 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <apex_queuing.h>
+#include <apex_sampling.h>
+#include <apex_partition.h>
 
-#include "kernel/circular_buffer.h"
+#include "../circular_buffer.h"
 
 
 /*
@@ -23,6 +26,7 @@
 	.circ_buf = {0, 0, 0, (message_size * nb_message) + \
 		(message_size * sizeof(size_t))}
 
+#define MAX_PROCESSES_PER_PARTITIONS 3
 
 typedef enum {
 	LEVEL_A,
@@ -30,12 +34,19 @@ typedef enum {
 	LEVEL_C,
 	LEVEL_D,
 	LEVEL_E,
-} CRITICALITY;
+} criticality_t;
 
 typedef enum {
-	SAMPLING_PORT,
-	QUEUING_PORT,
-} PORT_TYPE;
+	CODE,
+	DATA,
+	INPUT_OUTPUT,
+} mem_type_t;
+
+typedef enum {
+	READ_ONLY,
+	WRITE_ONLY,
+	READ_WRITE,
+} mem_access_t;
 
 struct queuing_port {
 	MESSAGE_RANGE_TYPE        MAX_NB_MESSAGE;
@@ -66,13 +77,23 @@ typedef struct {
 } port_t;
 
 typedef struct {
+	uint32_t                  stackpointer;
+	uint8_t                   exc_return_value;
+	uint32_t				  tickStamp;
+	PROCESS_STATUS_TYPE; /* unamed struct with -fms-extensions */
+} process_t;
+
+typedef struct {
 	PARTITION_ID_TYPE         IDENTIFIER;
 	NAME_TYPE                 partitionname;
-	CRITICALITY               criticality; /* Not used */
+	criticality_t             criticality; /* Not used */
 	bool                      systempartion; /* Not used*/
 	void                      (*entrypoint)(void);
-	size_t                    nb_ports;
+	APEX_INTEGER              nb_ports;
 	port_t                    *ports;
+	uint32_t                  nb_processes;
+	uint32_t                  index_running_process;
+	process_t                 processes[MAX_PROCESSES_PER_PARTITIONS];
 } partition_t;
 
 typedef struct{
@@ -82,17 +103,35 @@ typedef struct{
 	port_t                    **ports;
 } channel_t;
 
+typedef struct {
+	mem_type_t                type;
+	uint32_t                  size;
+	mem_access_t              access;
+	uint32_t                  address;
+} mem_req_t;
+
 typedef struct{
-	int partitionidentifier;
-    char partitionname[32];
-    float peroidseconds;
-    float perioddurationseconds;
-    const window_schedule *window_arr;
-} partition_schedule;
+	PARTITION_ID_TYPE         IDENTIFIER;
+	NAME_TYPE                 partitionname;
+	uint32_t                  arr_size;
+	mem_req_t                 *memory_arr;
+
+	/* This value could be calculated every time we make a new process,
+	   but this is just way easier... */
+	uint32_t                  mem_offset;
+} part_mem_t;
 
 typedef struct {
-    int windowidentifier;
-    float windowstartseconds;
-    float windowdurationseconds;
-    bool partitionperiodstart;
+	int windowidentifier;
+	float windowstartseconds;
+	float windowdurationseconds;
+	bool partitionperiodstart;
 } window_schedule;
+
+typedef struct{
+	int partitionidentifier;
+	char partitionname[32];
+	float peroidseconds;
+	float perioddurationseconds;
+	const window_schedule *window_arr;
+} partition_schedule;
