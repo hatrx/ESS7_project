a375a21bde80f40d6e08f08b7efcbfa8f6ac7839
Jakob Østergaard Jensen <jakob.jensen.91@gmail.com>
2016-12-05 13:19:31 +0100

    At least now it doesn't hardfault...

--- src/xml_data.c
+++ src/xml_data.c
@@ -6,41 +6,45 @@
 
 port_t p_dummy_1[1] = {{
     .is_queuing_port = true,
-    .q_buf = { MESSAGE_BUFFER(8, 30), },
-    .PORT_DIRECTION = SOURCE,
+    .q_buf = {
+		.PORT_DIRECTION = SOURCE,
+		MESSAGE_BUFFER(8, 30), },
     .portname = "print_1",
     },};
 
 port_t p_dummy_2[1] = {{
     .is_queuing_port = true,
-    .q_buf = { MESSAGE_BUFFER(8, 30), },
-    .PORT_DIRECTION = SOURCE,
+    .q_buf = {
+		.PORT_DIRECTION = SOURCE,
+		MESSAGE_BUFFER(8, 30), },
     .portname = "print_2",
     },};
 
 port_t p_stio_sys[1] = {{
     .is_queuing_port = true,
-    .q_buf = { MESSAGE_BUFFER(8, 30), },
-    .PORT_DIRECTION = DESTINATION,
+    .q_buf = {
+		.PORT_DIRECTION = DESTINATION,
+		MESSAGE_BUFFER(8, 30), },
     .portname = "sys_stio",
     },};
 
 port_t p_evil[1] = {{
     .is_queuing_port = true,
-    .q_buf = { MESSAGE_BUFFER(1, 1), },
-    .PORT_DIRECTION = DESTINATION,
+    .q_buf = {
+		.PORT_DIRECTION = DESTINATION,
+		MESSAGE_BUFFER(1, 1), },
     .portname = "temp",
     },};
 
 partition_t partitions[5] = {{
-    .IDENTIFIER = 0,
+    .id = 0,
     .partitionname = "idle",
     .criticality = LEVEL_E,
     .systempartion = false,
     .entrypoint = &idle_main,
     .nb_ports = 0,
     },{
-    .IDENTIFIER = 1,
+    .id = 1,
     .partitionname = "dummy_1",
     .criticality = LEVEL_A,
     .systempartion = false,
@@ -48,7 +52,7 @@ partition_t partitions[5] = {{
     .nb_ports = 1,
     .ports = p_dummy_1,
     },{
-    .IDENTIFIER = 2,
+    .id = 2,
     .partitionname = "dummy_2",
     .criticality = LEVEL_A,
     .systempartion = false,
@@ -56,7 +60,7 @@ partition_t partitions[5] = {{
     .nb_ports = 1,
     .ports = p_dummy_2,
     },{
-    .IDENTIFIER = 3,
+    .id = 3,
     .partitionname = "stio_sys",
     .criticality = LEVEL_A,
     .systempartion = true,
@@ -64,7 +68,7 @@ partition_t partitions[5] = {{
     .nb_ports = 1,
     .ports = p_stio_sys,
     },{
-    .IDENTIFIER = 4,
+    .id = 4,
     .partitionname = "evil",
     .criticality = LEVEL_A,
     .systempartion = false,
@@ -74,71 +78,71 @@ partition_t partitions[5] = {{
     },};
 
 const window_schedule windowp_dummy_1[2] = {{
-    .windowidentifier = 101,
+    .id = 101,
     .windowstartmilliseconds = 0,
     .windowdurationmilliseconds = 20,
     .partitionperiodstart = true,
     },{
-    .windowidentifier = 102,
+    .id = 102,
     .windowstartmilliseconds = 100,
     .windowdurationmilliseconds = 20,
     .partitionperiodstart = true,
     },};
 
 const window_schedule windowp_dummy_2[2] = {{
-    .windowidentifier = 201,
+    .id = 201,
     .windowstartmilliseconds = 30,
     .windowdurationmilliseconds = 10,
     .partitionperiodstart = true,
     },{
-    .windowidentifier = 202,
+    .id = 202,
     .windowstartmilliseconds = 130,
     .windowdurationmilliseconds = 10,
     .partitionperiodstart = true,
     },};
 
 const window_schedule windowp_stio_sys[2] = {{
-    .windowidentifier = 301,
+    .id = 301,
     .windowstartmilliseconds = 40,
     .windowdurationmilliseconds = 30,
     .partitionperiodstart = true,
     },{
-    .windowidentifier = 302,
+    .id = 302,
     .windowstartmilliseconds = 140,
     .windowdurationmilliseconds = 30,
     .partitionperiodstart = true,
     },};
 
 const window_schedule windowp_evil[1] = {{
-    .windowidentifier = 401,
+    .id = 401,
     .windowstartmilliseconds = 180,
     .windowdurationmilliseconds = 20,
     .partitionperiodstart = true,
     },};
 
 const partition_schedule partition_schedules[4] = {{
-    .partitionidentifier = 1,
+    .id = 1,
     .partitionname = "dummy_1",
     .peroidseconds = 0.100,
     .perioddurationseconds = 0.020,
     .numWindows = 2,
     .window_arr = windowp_dummy_1,
     },{
-    .partitionidentifier = 2,
+    .id = 2,
     .partitionname = "dummy_2",
     .peroidseconds = 0.100,
     .perioddurationseconds = 0.010,
     .numWindows = 2,
     .window_arr = windowp_dummy_2,
     },{
-    .partitionidentifier = 3,
+    .id = 3,
     .partitionname = "stio_sys",
     .peroidseconds = 0.100,
     .perioddurationseconds = 0.030,
     .numWindows = 2,
     .window_arr = windowp_stio_sys,
     },{
-    .partitionidentifier = 4,
+    .id = 4,
     .partitionname = "evil",
     .peroidseconds = 0.200,
     .perioddurationseconds = 0.020,
@@ -153,7 +157,7 @@ port_t *stio_channel_ports[3] = {
 };
 
 channel_t connection_table[1] = {{
-		.channelidentifier = 1,
+		.id = 1,
 		.channelname = "stio_channel",
 		.nb_ports = 3,
 		.ports = stio_channel_ports,
