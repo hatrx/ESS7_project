e360a90555174a87919658d69b45f563c036fcff
Jakob Østergaard Jensen <jakob.jensen.91@gmail.com>
2016-12-18 13:26:03 +0100

    renamed dummy partitions

--- src/partitions/yellow_toggler/yellow_toggler.c
+++ src/partitions/yellow_toggler/yellow_toggler.c
@@ -3,8 +3,7 @@
 
 #include <drivers/utils.h>
 #include <drivers/onboard_leds.h>
-#include <apex_types.h>
-#include <apex_time.h>
+#include <drivers/uart.h>
 
 #include <apex_queuing.h>
 
@@ -12,7 +11,7 @@
 static void worker(void);
 
 
-void dummy2_main(void)
+void yellow_toggler_main(void)
 {
     PROCESS_ATTRIBUTE_TYPE attributes = {
         .PERIOD = 0,
@@ -23,7 +22,7 @@ void dummy2_main(void)
         .DEADLINE = SOFT,
         .NAME = "worker",
     };
-
+    
     PROCESS_ID_TYPE processId;
     RETURN_CODE_TYPE RETURN_CODE;
     CREATE_PROCESS(&attributes, &processId, &RETURN_CODE);
@@ -32,25 +31,22 @@ void dummy2_main(void)
     while(1);
 }
 
-
 void worker(void)
 {
     QUEUING_PORT_ID_TYPE QUEUING_PORT_ID;
     RETURN_CODE_TYPE RETURN_CODE;
-    CREATE_QUEUING_PORT("print_2", 32, 32, SOURCE, FIFO, &QUEUING_PORT_ID, &RETURN_CODE);
-    SYSTEM_TIME_TYPE timeNow;
-    RETURN_CODE_TYPE returnCode;
+    CREATE_QUEUING_PORT("yellow_print", 32, 32, SOURCE, FIFO, &QUEUING_PORT_ID, &RETURN_CODE);
+
 
     while (1) {
-        char *str = "Dummy 2\n\r";
+        char *str = "Yellow toggler\n\r";
         size_t len = strlen(str);
         RETURN_CODE_TYPE RETURN_CODE;
         SEND_QUEUING_MESSAGE(QUEUING_PORT_ID, (uint8_t *)str, len, 0, &RETURN_CODE);
 
         for (size_t i = 0; i < 8; i++) {
-            onboard_led_toggle(red_led);
+            onboard_led_toggle(yellow_led);
             delay_ms(100);
         }
-        GET_TIME(&timeNow, &returnCode);
     }
 }
