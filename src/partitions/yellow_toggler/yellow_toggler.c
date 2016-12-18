9fa148ea9058e9df290d6e13ae5170d8964ca49d
Jakob Østergaard Jensen <jakob.jensen.91@gmail.com>
2016-12-18 14:54:12 +0100

    Fixed line length

--- src/partitions/yellow_toggler/yellow_toggler.c
+++ src/partitions/yellow_toggler/yellow_toggler.c
@@ -35,14 +35,16 @@ void worker(void)
 {
     QUEUING_PORT_ID_TYPE QUEUING_PORT_ID;
     RETURN_CODE_TYPE RETURN_CODE;
-    CREATE_QUEUING_PORT("yellow_print", 32, 32, SOURCE, FIFO, &QUEUING_PORT_ID, &RETURN_CODE);
+    CREATE_QUEUING_PORT("yellow_print", 32, 32, SOURCE, FIFO,
+		&QUEUING_PORT_ID, &RETURN_CODE);
 
 
     while (1) {
         char *str = "Yellow toggler\n\r";
         size_t len = strlen(str);
         RETURN_CODE_TYPE RETURN_CODE;
-        SEND_QUEUING_MESSAGE(QUEUING_PORT_ID, (uint8_t *)str, len, 0, &RETURN_CODE);
+        SEND_QUEUING_MESSAGE(QUEUING_PORT_ID, (uint8_t *)str,
+			len, 0,	&RETURN_CODE);
 
         for (size_t i = 0; i < 8; i++) {
             onboard_led_toggle(yellow_led);
