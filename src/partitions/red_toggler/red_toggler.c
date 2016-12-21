eb138f57d55fa7c8b331e40164e561739b224262
Jakob Østergaard Jensen <jakob.jensen.91@gmail.com>
2016-12-21 11:10:13 +0100

    fix

--- src/partitions/red_toggler/red_toggler.c
+++ src/partitions/red_toggler/red_toggler.c
@@ -47,7 +47,7 @@ void worker(void)
         RETURN_CODE_TYPE RETURN_CODE;
         SEND_QUEUING_MESSAGE(QUEUING_PORT_ID, (uint8_t *)str, len, 0, &RETURN_CODE);
 
-        for (size_t i = 0; i < 20; i++) {
+        for (size_t i = 0; i < 10; i++) {
             onboard_led_toggle(red_led);
             delay_ms(100);
         }
--- src/partitions/yellow_toggler/yellow_toggler.c
+++ src/partitions/yellow_toggler/yellow_toggler.c
@@ -22,7 +22,7 @@ void yellow_toggler_main(void)
         .DEADLINE = SOFT,
         .NAME = "worker",
     };
-    
+
     PROCESS_ID_TYPE processId;
     RETURN_CODE_TYPE RETURN_CODE;
     CREATE_PROCESS(&attributes, &processId, &RETURN_CODE);
@@ -46,7 +46,7 @@ void worker(void)
         SEND_QUEUING_MESSAGE(QUEUING_PORT_ID, (uint8_t *)str,
 			len, 0,	&RETURN_CODE);
 
-        for (size_t i = 0; i < 20; i++) {
+        for (size_t i = 0; i < 10; i++) {
             onboard_led_toggle(yellow_led);
             delay_ms(100);
         }
